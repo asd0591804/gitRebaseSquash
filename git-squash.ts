a73386a1e421a3213a47e8ac49ba9b689887f351
BingHua Lai <binghua.lai@hepiuscare.com.tw>
2023-09-07 18:00:53 +0800

    merge test
    third line
    
    double line

--- git-squash.ts
+++ git-squash.ts
@@ -1,2 +1,7 @@
 console.log('first squash test');
 console.log('second squash test');
+console.log('third squash test');
+
+console.log('first squash test');
+console.log('second squash test');
+console.log('third squash test');
