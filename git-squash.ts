c7157a1279f02407af00fe5a5c6323d207d56679
BingHua Lai <binghua.lai@hepiuscare.com.tw>
2023-09-07 18:08:56 +0800

    triple line

--- git-squash.ts
+++ git-squash.ts
@@ -5,3 +5,7 @@ console.log('third squash test');
 console.log('first squash test');
 console.log('second squash test');
 console.log('third squash test');
+
+console.log('first squash test');
+console.log('second squash test');
+console.log('third squash test');
